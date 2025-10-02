9f46f8c869b49018b5ab4a0d06a06dea5dae2b28
Nycolas <rodriguesnycolas22@outlook.com>
2025-10-02 14:14:56 -0300

    Update route.jsx

--- API-FRONT/cadastro-front/src/rotas/route.jsx
+++ API-FRONT/cadastro-front/src/rotas/route.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-// Importa suas telas
+
 import Welcome from '../pages/welcome/welcome';
 import Home from '../pages/home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// O componente AppRoutes/Router agora está em seu próprio arquivo
+
 function AppRoutes() {
   return (
     <BrowserRouter>
